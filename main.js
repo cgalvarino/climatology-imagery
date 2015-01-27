738f790767835e697143f2882a14aa79f7fff9fe
Charlton Galvarino <charlton@2creek.com>
2015-01-27 14:01:11 -0500

    custom colors

--- main.js
+++ main.js
@@ -462,7 +462,7 @@ function query(customRange) {
 
   dataTable.clear();
 
-  var td = ['<td>' + '<button type="button" class="btn btn-default btn-sm margin-bottom"><span class="glyphicon glyphicon-plus-sign aqua"></span> New query</button></div><br>Click above to look for seasonal trends at a particular location.' + '</td>'];
+  var td = ['<td>' + '<button type="button" class="btn btn-default btn-sm margin-bottom"><span class="glyphicon glyphicon-plus-sign aqua"></span> New query</button></div><br>Click \'New query\' to look for seasonal trends at a particular location.' + '</td>'];
   _.each(intervals,function(i) {
     var img = {id : 'foo',src: 'img/blank.png'};
     if (_.indexOf($('#intervals select').selectpicker('val'),i) >= 0 && years) {
@@ -510,7 +510,9 @@ function query(customRange) {
           ,'https://chart.googleapis.com/chart?chxt=x,y&cht=bvs&chd=t:'
             + o[1].join(',')
             + '&chds=' + (minVal * 0.85) + ',' + (maxVal * 1.15)
-            + '&chco=a9d2dc&chs=150x150&chxl=0:|' + yrsStr.join('|')
+            // supporting up to 12 custom colors
+            + '&chco=8dd3c7|ffffb3|bebada|fb8072|80b1d3|fdb462|b3de69|fccde5|d9d9d9|bc80bd|ccebc5|ffed6f'
+            + '&chs=150x150&chxl=0:|' + yrsStr.join('|')
             + '&chxs=0,808080,12,0,_|1,000000,0,0,_&chm=N,333333,0,,9&chbh=15'
         );
       });
